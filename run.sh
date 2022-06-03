b8d1bb4bbc7fb0d36e2a0193a576dc66c144f6a0
xna <xna00@qq.com>
2022-06-04 00:15:55 +0800

    feat: run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 git pull
-cd client/web
-rm package-lock.json
-npm i
-npm run build
-cd ../../server
-rm package-lock.json
-npm i
-tsc
-cd ..
-node server/dist/server/main.js
+cd server
+pnpm i
+npm run dev &
+cd ../client/mobile
+pnpm i
+npm run dev &
